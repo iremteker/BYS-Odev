c7dbc219c23d60f1cdca547337399edd758eaf16
İrem Teker <147082141+iremteker@users.noreply.github.com>
2024-12-20 18:05:55 +0300

    student dashboard

--- obj/Debug/net8.0/SchoolManagementSystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/SchoolManagementSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7236c1650debdb1f73696db79daa3bc3436f0820")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+445d5cf0124507b99bde659b675d1eca149cad4e")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolManagementSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
